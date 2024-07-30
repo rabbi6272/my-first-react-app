ce38dcdd8915786d3227ef004a24e2fff4d28801
Rabbi <140103650+rabbi6272@users.noreply.github.com>
2024-07-30 15:49:50 +0600

    ADDED NEW FEATURE V1.0.0

--- src/components/todo.js
+++ src/components/todo.js
@@ -4,9 +4,10 @@ import "./todo.css";
 
 export default function Todo() {
   const [items, setItems] = useState([]);
+  const [sort, setSort] = useState("input");
 
   function handleSetItems(item) {
-    setItems([...items, item]);
+    setItems([item, ...items]);
   }
 
   function handleDeleteItems(id) {
@@ -20,15 +21,40 @@ export default function Todo() {
       )
     );
   }
+
+  function handleSortItems(e) {
+    setSort(e.target.value);
+  }
+
+  function handleResetItems() {
+    setItems([]);
+  }
+
+  let sortedItems;
+  if (sort === "input") {
+    sortedItems = items;
+  } else if (sort === "description") {
+    sortedItems = items.slice().sort((a, b) => a.name.localeCompare(b.name));
+  } else {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.completed) - Number(b.completed));
+  }
+
   return (
     <div className="wrapper p-4 bg-slate-700 rounded-lg flex flex-col gap-4 h-3/5 md:h-2/4 lg:w-2/4 md:w-3/5 ">
       <SetItems handleSetItems={handleSetItems} />
       <ShowItems
-        items={items}
+        items={sortedItems}
         handleDeleteItems={handleDeleteItems}
         handleUpdateItems={handleUpdateItems}
       />
-      <ShowStats items={items} />
+      <ShowStats
+        items={items}
+        handleSortItems={handleSortItems}
+        sort={sort}
+        handleResetItems={handleResetItems}
+      />
     </div>
   );
 }
@@ -101,15 +127,31 @@ function ShowItems({ items, handleDeleteItems, handleUpdateItems }) {
   );
 }
 
-function ShowStats({ items }) {
+function ShowStats({ items, sort, handleSortItems, handleResetItems }) {
   if (!items.length) return <p className="stats">No items</p>;
 
   const totalItems = items.length;
   const completedItems = items.filter((item) => item.completed).length;
   const percentage = Math.round((completedItems / totalItems) * 100);
   return (
-    <p className="stats">
-      Total items: {totalItems}, completed: {completedItems} ({percentage}%)
-    </p>
+    <div className="stats">
+      <div className="flex gap-4">
+        <select
+          className="text-center rounded-lg text-white px-2 outline-none bg-slate-600"
+          value={sort}
+          onChange={handleSortItems}
+        >
+          <option value="input">Input order</option>
+          <option value="description">Description</option>
+          <option value="completed">Completed</option>
+        </select>
+        <Button variant="contained" onClick={() => handleResetItems()}>
+          RESET
+        </Button>
+      </div>
+      <p>
+        Total items: {totalItems}, completed: {completedItems} ({percentage}%)
+      </p>
+    </div>
   );
 }
